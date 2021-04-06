d32fded75d8a2227dc109d27c8953dbe21268a07
Evyuel <drugban1994@gmail.com>
2021-04-06 20:09:14 +0300

    Покрытие тестами

---
+++ spring-04junit/src/main/test/ru/otus/spring/domain/QuestioneerTest.java
@@ -0,0 +1,30 @@
+package ru.otus.spring.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestioneerTest {
+
+    @DisplayName("Должен корректно присваивать имя стунденту")
+    @Test
+    void setStudentFIO() {
+        Questioneer questioneer = new Questioneer();
+    }
+
+    @Test
+    void getStudentFIO() {
+    }
+
+    @Test
+    void increaseCounter() {
+    }
+
+    @Test
+    void getResult() {
+    }
+}
