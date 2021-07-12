2f0effb5f3a7d930eb7a0b2bdf10d96cce676676
Evyuel <drugban1994@gmail.com>
2021-07-12 20:43:16 +0300

    Задание 13

---
+++ spring-24/src/main/resources/db/migration/data/R__02_Add_UserAuthorities.sql
@@ -0,0 +1,3 @@
+insert into UserAuthorities(id,userid,authority) values (1,1,'ADMIN');
+insert into UserAuthorities(id,userid,authority) values (2,2,'USER');
+
---
+++ spring-24/src/test/java/ru/dtimofeev/springapp/repositories/BookJpaTest.java
@@ -0,0 +1,58 @@
+package ru.dtimofeev.springapp.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import ru.dtimofeev.springapp.models.Author;
+import ru.dtimofeev.springapp.models.Book;
+import ru.dtimofeev.springapp.models.BookComment;
+import ru.dtimofeev.springapp.models.Genre;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+@DisplayName("Класс BookJpa должен")
+class BookJpaTest {
+
+    private static final Genre FIRST_BOOK_GENRE = new Genre(1, "Классика");
+    private static final List<Author> LIST_OF_AUTHORS = List.of(new Author(3, "Агата Кристи"), new Author(1, "Михаил Булгаков"));
+    private static final List<String> LIST_OF_AUTHORS_NAME = List.of("Агата Кристи", "Михаил Булгаков");
+    private static final List<String> LITS_OF_BOOK_COMMENTS_STRING = List.of("TopComment1", "TopComment2");
+    private static final Book BOOK_FOR_SAVE = new Book(0, "TestBook", FIRST_BOOK_GENRE, LIST_OF_AUTHORS);
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+    @Autowired
+    private BookRepository bookRepository;
+
+    @DisplayName(" сохранять книгу")
+    @Test
+    void shouldCorrectSaveBook() {
+        Book savedBook = bookRepository.save(BOOK_FOR_SAVE);
+        List<BookComment> LIST_OF_BOOK_COMMENTS = List.of(new BookComment(0, "TopComment1", savedBook), new BookComment(0, "TopComment2", savedBook));
+        savedBook.setBookComments(LIST_OF_BOOK_COMMENTS);
+        testEntityManager.flush();
+        testEntityManager.clear();
+
+        Book findedBook = testEntityManager.find(Book.class, savedBook.getId());
+        List<String> commentsString = new ArrayList<>();
+        for (BookComment bc : findedBook.getBookComments()) {
+            commentsString.add(bc.getCommentText());
+        }
+        List<String> authorsName = new ArrayList<>();
+        for (Author a : findedBook.getAuthors()) {
+            authorsName.add(a.getFullName());
+        }
+
+        assertAll(() -> findedBook.getName().equals("TestBook"),
+                () -> findedBook.getGenre().getName().equals("Классика"));
+        assertThat(LIST_OF_AUTHORS_NAME).isEqualTo(authorsName);
+        assertThat(LITS_OF_BOOK_COMMENTS_STRING).isEqualTo(commentsString);
+    }
+}
